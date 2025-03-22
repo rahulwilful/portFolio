2515ed0d2f3c22f5ec3e9c4f7dd53885334132fa
rahul patil <rahulwilful@gmail.com>
2025-03-22 23:49:06 +0530

    22/03/2025

--- src/constants/ProjectInformation.js
+++ src/constants/ProjectInformation.js
@@ -4,7 +4,7 @@ export const projectInformation = [
     from: 'September 2024',
     to: 'November 2024',
     organisation: 'Freelance',
-    skills: ['React.js', 'Node.js', 'SQL', 'MySql', 'Express.js', 'Bootstrap (Framework)', 'Cascading Style Sheets (CSS)', 'GSAP', 'GitHub', 'phpMyadmin'],
+    skills: ['React', 'Node', 'SQL', 'MySql', 'Express', 'Bootstrap', 'CSS', 'GSAP', 'GitHub', 'phpMyadmin'],
     details: ['Built an analytics dashboard to display and analyze game data for seven casino games, including roulette, poker, and baccarat.', 'Used graphs, tables, and charts to present winning numbers, player vs. dealer stats, and card outcomes.', 'Implemented mathematical logic like Standard Deviation, Chi Test for accurate results visualization.']
   },
   {
@@ -12,7 +12,7 @@ export const projectInformation = [
     from: 'April 2024',
     to: 'May 2024',
     organisation: 'Tripytoe',
-    skills: ['Vue.js', 'Node.js', 'MongoDB', 'Express.js', 'Bootstrap (Framework)', 'Cascading Style Sheets (CSS)', 'Facebook for Business', 'GitHub'],
+    skills: ['Vue', 'Node', 'MongoDB', 'Express', 'Bootstrap', 'CSS', 'Facebook for Business', 'GitHub'],
     details: ['Developed an AI model to translate English to Konkani, requiring a large dataset of sentences in English and their translations in Konkani to train the model.', 'Built a system to send English sentences from Google Sheets to users via WhatsApp automatically, storing replies in both a database and Google Sheet.', "Translators could start and stop messages by typing 'start' and 'stop' on WhatsApp."]
   },
   {
@@ -20,7 +20,7 @@ export const projectInformation = [
     from: 'May 2024',
     to: 'May 2024',
     organisation: 'Tripytoe',
-    skills: ['Node.js', 'Vue.js', 'Express.js', 'SQL', 'Cascading Style Sheets (CSS)', 'Bootstrap (Framework)', 'HTML5', 'GitHub'],
+    skills: ['Node', 'Vue', 'Express', 'SQL', 'CSS', 'Bootstrap', 'HTML5', 'GitHub'],
     details: ['Rebuilt the Konkani WordNet website, a lexical database for the Konkani language, designed and developed by the Department of Computer Science and Technology, Goa University, Goa.']
   },
   {
@@ -28,7 +28,7 @@ export const projectInformation = [
     from: 'March 2024',
     to: 'March 2024',
     organisation: 'Tripytoe',
-    skills: ['Vue.js', 'Node.js', 'MongoDB', 'Express.js', 'Bootstrap (Framework)', 'Cascading Style Sheets (CSS)', 'GitHub'],
+    skills: ['Vue', 'Node', 'MongoDB', 'Express', 'Bootstrap', 'CSS', 'GitHub'],
     details: ['Created a Post Generator Web App for a local news agency.', 'Users can input an image, tagline, heading, and news content.', 'Based on the input, a news post will be generated, which can be downloaded as an image to post on on different social media platforms.']
   },
   {
@@ -36,7 +36,7 @@ export const projectInformation = [
     from: 'April 2024',
     to: 'April 2024',
     organisation: 'Akanksha Surgicals',
-    skills: ['Vue.js', 'Node.js', 'MongoDB', 'Express.js', 'Bootstrap (Framework)', 'Cloudinary', 'Version Control', 'GitHub'],
+    skills: ['Vue', 'Node', 'MongoDB', 'Express', 'Bootstrap', 'Cloudinary', 'Version Control', 'GitHub'],
     details: ['Developed a Medical Store Website for Akanksha Surgicals.', 'Here I used vueJs for frontend , nodeJs and express for backend and mongoDb for database for storing and retrieving data Cloudinary for images.', 'Features include user sign up/sign in, profile management, admin dashboard, product management, email verification, and more.']
   },
 
@@ -45,7 +45,7 @@ export const projectInformation = [
     from: 'July 2022',
     to: 'June 2023',
     organisation: 'Agnel Institute Of Technology And Design',
-    skills: ['PHP', 'Arduino Programming', 'HTML5', 'Cascading Style Sheets (CSS)', 'Bootstrap (Framework)', 'Node-RED', 'MySQL', 'GitHub'],
+    skills: ['PHP', 'Arduino Programming', 'HTML5', 'CSS', 'Bootstrap', 'Node-RED', 'MySQL', 'GitHub'],
     details: ['Led a team to automate a water meter with Arduino Uno.', 'Designed a system to record meter readings and send data to a server for billing.', 'Programmed Arduino Uno in C/C++ and integrated other technologies for data management and bill generation.']
   }
 ];
